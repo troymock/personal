f308d8f92b64ae392d3dd664ecbd4e58e4d7e65b
Troy Mock <tcm87@cornell.edu>
2016-02-06 14:22:02 -0500

    image compression

--- config.rb
+++ config.rb
@@ -87,3 +87,18 @@ configure :build do
 
   # activate :imageoptim
 end
+
+# To deploy to a remote branch via git (e.g. gh-pages on github):
+activate :deploy do |deploy|
+    deploy.method = :git
+    # remote is optional (default is "origin")
+    # run `git remote -v` to see a list of possible remotes
+    deploy.remote = "some-other-remote-name"
+    
+    # branch is optional (default is "gh-pages")
+    # run `git branch -a` to see a list of possible branches
+    deploy.branch = "some-other-branch-name"
+    
+    # strategy is optional (default is :force_push)
+    deploy.strategy = :submodule
+end
--- source/javascripts/menu.js
+++
@@ -1,9 +0,0 @@
-function mobileMenu(){
-  console.log($('.site-header').height());
-  if ($('.site-header').height() < 120){
-    $('.site-header').animate({height: "143px"}, 300);
-  }
-  else {
-    $('.site-header').animate({height: "92px"}, 300);
-  }
-}
